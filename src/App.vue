97cdba6951e64e4b5c86bd4c12fd48cf9d3eb49b
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2021-12-30 12:39:01 -0300

    header init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderAndSidebar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderAndSidebar from './components/HeaderAndSidebar/HeaderAndSidebar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderAndSidebar
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family:'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/HeaderAndSidebar/HeaderAndSidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav class="navbar navbar-light bg-light border">
+      <div class="container-fluid col-3">
+        <a class="navbar-brand" href="#">Logo</a>
+      </div>
+            <div class="col-6">Espaçamento</div>
+            <div class="col-3 text-end">
+        <a class="navbar-brand" href="#">Info cliente</a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderAndSidebar",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" src="./style.scss"></style>
