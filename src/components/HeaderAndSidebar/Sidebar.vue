0808f6b986db34c3ac8c67ab7d5ae88c21ad872b
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2022-01-15 20:20:52 -0300

    li definition

--- src/components/HeaderAndSidebar/Sidebar.vue
+++ src/components/HeaderAndSidebar/Sidebar.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="col-2 container-sidebar">
-    <ul class="list-group li-group">
+    <ul class="list-group">
       <li class="list-group-item">
         <fa icon="chart-line" class="icon" /><span>Dashboard</span>
       </li>
     </ul>
-    <ul class="list-group li-group">
+    <ul class="list-group register">
       <li class="list-group-item">
-        <fa icon="id-card" class="icon" /><span>Cadastro</span>
+        <fa icon="id-card" class="icon" /><span class="subtitle">Cadastro</span>
       </li>
-      <li class="list-group-item">
-        <fa icon="arrow-right" class="icon" /><span>Clientes</span>
+      <li class="list-group-item ps-4">
+        <span class="arrow">>></span><span>Clientes</span>
       </li>
-      <li class="list-group-item">
-        <fa icon="arrow-right" class="icon" /><span>Operadoras</span>
+      <li class="list-group-item ps-4">
+        <span class="arrow">>></span><span>Operadoras</span>
       </li>
+      <li class="list-group-item ps-4">
+        <span class="arrow">>></span><span>Usuários</span>
+      </li>
+    </ul>
+        <ul class="list-group">
       <li class="list-group-item">
-        <fa icon="arrow-right" class="icon" /><span>Usuários</span>
+        <fa icon="sign-out-alt" class="icon" /><span>Sair</span>
       </li>
     </ul>
   </div>
