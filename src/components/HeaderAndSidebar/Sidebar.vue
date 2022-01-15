42d0735bbbed6466fc8d449964076bf32b077764
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2022-01-15 14:44:55 -0300

    sidbar init create

--- src/components/HeaderAndSidebar/Sidebar.vue
+++ src/components/HeaderAndSidebar/Sidebar.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    <h1>sidebar</h1>
+  <div class="col-2 container-sidebar">
+    <ul class="list-group li-group">
+      <li class="list-group-item">
+        <fa icon="chart-line" class="icon" /><span>Dashboard</span>
+      </li>
+    </ul>
+    <ul class="list-group li-group">
+      <li class="list-group-item">
+        <fa icon="id-card" class="icon" /><span>Cadastro</span>
+      </li>
+      <li class="list-group-item">
+        <fa icon="arrow-right" class="icon" /><span>Clientes</span>
+      </li>
+      <li class="list-group-item">
+        <fa icon="arrow-right" class="icon" /><span>Operadoras</span>
+      </li>
+      <li class="list-group-item">
+        <fa icon="arrow-right" class="icon" /><span>Usuários</span>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -8,4 +26,11 @@
 export default {
   name: "Sidebar",
 };
+/* methods: {
+  alert("teste");
+} */
 </script>
+
+
+
+<style scoped lang="scss" src="./scss/sidebar.scss"></style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from "./App.vue";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPhone, faIdCard } from "@fortawesome/free-solid-svg-icons";
+import { faPhone, faIdCard, faChartLine, faClipboardList, faQuestionCircle, faSignOutAlt, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faPhone, faIdCard);
+library.add(faPhone, faIdCard, faChartLine, faClipboardList, faQuestionCircle, faSignOutAlt, faArrowRight);
 
 createApp(App).component("fa", FontAwesomeIcon).mount("#app");
