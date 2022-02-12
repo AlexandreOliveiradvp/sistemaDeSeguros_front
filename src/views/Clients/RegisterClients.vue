745928f5bad7bfcaeb03dddc5176689d8f07d880
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2022-02-12 20:44:16 -0300

    Register clients screen start

--- src/views/Clients/RegisterClients.vue
+++ src/views/Clients/RegisterClients.vue
@@ -6,15 +6,78 @@
           <div class="col">
             <h1 class="text-secondary pt-2">Cadastrar Clientes</h1>
             <div class="d-flex">
-                <router-link class="link" to="/Clientes"><p class="text-secondary">Clientes</p></router-link>
-                <p class="arrow-link">>></p>
-                <router-link class="link" to="/CadastrarClientes"><p class="text-secondary">Cadastrar Clientes</p></router-link>
+              <router-link class="link" to="/Clientes"
+                ><p class="text-secondary">Clientes</p></router-link
+              >
+              <p class="arrow-link">>></p>
+              <router-link class="link" to="/CadastrarClientes"
+                ><p class="text-secondary">Cadastrar Clientes</p></router-link
+              >
             </div>
           </div>
           <div class="col">
-            <button class="btn float-end btn-clients">
-              Salvar Cliente
-            </button>
+            <button class="btn float-end btn-clients">Salvar Cliente</button>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="card mt-2">
+            <div class="card-header bg-white text-secondary">Dados Usuário</div>
+              <div class="card-body ">
+                <div class="col-12 d-flex">
+                  <div class="col-md-12 col-lg-3 pe-2">
+                    <label class="form-label label-input text-secondary">Nome:</label>
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control" placeholder="Digite seu nome">
+                    </div>
+                  </div>
+                  <div class="col-md-12 col-lg-3 ps-2 pe-2">
+                    <label class="form-label label-input text-secondary">Email:</label>
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control" placeholder="Digite seu email">
+                    </div>
+                  </div>
+                  <div class="col-md-12 col-lg-3 ps-2 pe-2">
+                    <label class="form-label label-input text-secondary">CPF:</label>
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control" placeholder="Digite seu CPF">
+                    </div>
+                  </div>
+                  <div class="col-md-12 col-lg-3 ps-2">
+                    <label class="form-label label-input text-secondary">Telefone:</label>
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control" placeholder="Digite seu telefone">
+                    </div>
+                  </div>
+                </div>
+                <div class="col-12 d-flex">
+                  <div class="col-md-12 col-lg-3 pe-2">
+                    <label class="form-label label-input text-secondary">Nome de Usuário:</label>
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control" placeholder="Digite o nome">
+                    </div>
+                  </div>
+                  <div class="col-md-12 col-lg-3 ps-2 pe-2">
+                    <label class="form-label label-input text-secondary">Senha:</label>
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control" placeholder="Digite a senha">
+                    </div>
+                  </div>
+                  <div class="col-md-12 col-lg-3 ps-2 pe-2">
+                    <label class="form-label label-input text-secondary">Tipo:</label>
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control">
+                    </div>
+                  </div>
+                  <div class="col-md-12 col-lg-3 ps-2">
+                    <label class="form-label label-input text-secondary">Departamento:</label>
+                    <div class="input-group mb-3">
+                      <input type="text" class="form-control">
+                    </div>
+                  </div>
+                </div>
+              </div>
           </div>
         </div>
       </div>
